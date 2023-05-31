3bbf782c2a2f5a9b07e2497bcec0d6cdd3cc549b
Hesham Gasser <heshamgasser32@gmail.com>
2023-06-01 02:49:08 +0300

    add singleChildScrollView

--- lib/screens/widget/add_task/edit_note_modalSheet.dart
+++ lib/screens/widget/add_task/edit_note_modalSheet.dart
@@ -48,144 +48,146 @@ class _EditTaskModalSheetState extends State<EditTaskModalSheet> {
                 left: 10,
                 right: 10,
                 bottom: MediaQuery.of(context).viewInsets.bottom),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Center(
-                  child: Text(
-                    AppLocalizations.of(context)!.editTask,
-                    style: Theme.of(context).textTheme.bodyLarge,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Center(
+                    child: Text(
+                      AppLocalizations.of(context)!.editTask,
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
                   ),
-                ),
-                Divider(
-                  thickness: 3,
-                  color: Theme.of(context).colorScheme.onBackground,
-                  indent: MediaQuery.of(context).size.width * .35,
-                  endIndent: MediaQuery.of(context).size.width * .35,
-                ),
-                SizedBox(height: 10),
-                CustomEditTextFormField(
-                  hint: AppLocalizations.of(context)!.taskTitle,
-                  controller: taskTitleController,
-                ),
-                SizedBox(height: 10),
-                CustomEditTextFormField(
-                  hint: AppLocalizations.of(context)!.taskDetail,
-                  maxLines: 5,
-                  controller: descriptionController,
-                ),
-                SizedBox(height: 10),
-                Row(
-                  children: [
-                    Switch(
-                      activeColor: Theme.of(context).primaryColor,
-                      value: active,
-                      onChanged: (value) {
-                        setState(() {
-                          active = value;
-                          print(active);
-                        });
-                      },
-                    ),
-                    SizedBox(width: 10),
-                    Text(
-                      AppLocalizations.of(context)!.updateDateTime,
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    ),
-                  ],
-                ),
-                SizedBox(height: 10),
-                Text(AppLocalizations.of(context)!.taskDateTime,
-                    style: Theme.of(context).textTheme.bodyMedium),
-                SizedBox(
-                  height: 10,
-                ),
-                AbsorbPointer(
-                  absorbing: !active,
-                  child: Row(
+                  Divider(
+                    thickness: 3,
+                    color: Theme.of(context).colorScheme.onBackground,
+                    indent: MediaQuery.of(context).size.width * .35,
+                    endIndent: MediaQuery.of(context).size.width * .35,
+                  ),
+                  SizedBox(height: 10),
+                  CustomEditTextFormField(
+                    hint: AppLocalizations.of(context)!.taskTitle,
+                    controller: taskTitleController,
+                  ),
+                  SizedBox(height: 10),
+                  CustomEditTextFormField(
+                    hint: AppLocalizations.of(context)!.taskDetail,
+                    maxLines: 5,
+                    controller: descriptionController,
+                  ),
+                  SizedBox(height: 10),
+                  Row(
                     children: [
-                      Expanded(
-                        child: DateTimeWidget(
-                          childText: active == false
-                              ? widget.taskModel.date
-                              : dateTimeProvider.date
-                                  .toString()
-                                  .substring(0, 10),
-                          onTapped: () {
-                            dateTimeProvider.onDateTapped(context);
-                          },
-                          checkColor: !active,
-                        ),
+                      Switch(
+                        activeColor: Theme.of(context).primaryColor,
+                        value: active,
+                        onChanged: (value) {
+                          setState(() {
+                            active = value;
+                            print(active);
+                          });
+                        },
                       ),
                       SizedBox(width: 10),
+                      Text(
+                        AppLocalizations.of(context)!.updateDateTime,
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Text(AppLocalizations.of(context)!.taskDateTime,
+                      style: Theme.of(context).textTheme.bodyMedium),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  AbsorbPointer(
+                    absorbing: !active,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: DateTimeWidget(
+                            childText: active == false
+                                ? widget.taskModel.date
+                                : dateTimeProvider.date
+                                    .toString()
+                                    .substring(0, 10),
+                            onTapped: () {
+                              dateTimeProvider.onDateTapped(context);
+                            },
+                            checkColor: !active,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Expanded(
+                          child: DateTimeWidget(
+                            childText: active == false
+                                ? widget.taskModel.time
+                                : dateTimeProvider.initialTime
+                                    .format(context)
+                                    .toString(),
+                            onTapped: () {
+                              dateTimeProvider.onTimeTapped(context);
+                            },
+                            checkColor: !active,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
                       Expanded(
-                        child: DateTimeWidget(
-                          childText: active == false
-                              ? widget.taskModel.time
-                              : dateTimeProvider.initialTime
-                                  .format(context)
-                                  .toString(),
+                        child: CustomElevatedButton(
+                          backgroundColor: Theme.of(context).primaryColor,
+                          buttonText: AppLocalizations.of(context)!.update,
                           onTapped: () {
-                            dateTimeProvider.onTimeTapped(context);
+                            if (editFormKey.currentState!.validate()) {
+                              TaskModel updatedTask = TaskModel(
+                                  id: widget.taskModel.id,
+                                  title: taskTitleController.text,
+                                  description: descriptionController.text,
+                                  date: active == false
+                                      ? widget.taskModel.date
+                                      : dateTimeProvider.date
+                                          .toString()
+                                          .substring(0, 10),
+                                  time: active == false
+                                      ? widget.taskModel.time
+                                      : dateTimeProvider.initialTime
+                                          .format(context)
+                                          .toString(),
+                                  status: status);
+                              FirebaseFunctions.updateTask(
+                                      widget.taskModel.id, updatedTask)
+                                  .then(
+                                (value) {
+                                  Navigator.pop(context);
+                                },
+                              );
+                            }
                           },
-                          checkColor: !active,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                        child: CustomElevatedButton(
+                          backgroundColor: Theme.of(context).colorScheme.error,
+                          buttonText: AppLocalizations.of(context)!.cancel,
+                          onTapped: () {
+                            Navigator.pop(context);
+                          },
                         ),
                       ),
                     ],
                   ),
-                ),
-                SizedBox(height: 20),
-                Row(
-                  children: [
-                    Expanded(
-                      child: CustomElevatedButton(
-                        backgroundColor: Theme.of(context).primaryColor,
-                        buttonText: AppLocalizations.of(context)!.update,
-                        onTapped: () {
-                          if (editFormKey.currentState!.validate()) {
-                            TaskModel updatedTask = TaskModel(
-                                id: widget.taskModel.id,
-                                title: taskTitleController.text,
-                                description: descriptionController.text,
-                                date: active == false
-                                    ? widget.taskModel.date
-                                    : dateTimeProvider.date
-                                        .toString()
-                                        .substring(0, 10),
-                                time: active == false
-                                    ? widget.taskModel.time
-                                    : dateTimeProvider.initialTime
-                                        .format(context)
-                                        .toString(),
-                                status: status);
-                            FirebaseFunctions.updateTask(
-                                    widget.taskModel.id, updatedTask)
-                                .then(
-                              (value) {
-                                Navigator.pop(context);
-                              },
-                            );
-                          }
-                        },
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Expanded(
-                      child: CustomElevatedButton(
-                        backgroundColor: Theme.of(context).colorScheme.error,
-                        buttonText: AppLocalizations.of(context)!.cancel,
-                        onTapped: () {
-                          Navigator.pop(context);
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 15),
-              ],
+                  SizedBox(height: 15),
+                ],
+              ),
             ),
           ),
         );
